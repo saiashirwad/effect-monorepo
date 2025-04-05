b246b069751d2aeb5db627a10728896d07e34ac5
lucas-barake <86905052+lucas-barake@users.noreply.github.com>
2025-04-04 21:29:23 -0500

    fix eslint

--- packages/client/src/root.tsx
+++ packages/client/src/root.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from "react";
+import React from "react";
 import { createRoot } from "react-dom/client";
 import { GlobalProviders } from "./global-providers.tsx";
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+  <React.StrictMode>
     <GlobalProviders />
-  </StrictMode>,
+  </React.StrictMode>,
 );
